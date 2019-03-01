acd2bdfbc5c0dcd2afa8c5f81d8b87641b8e5cf3
Y16M035 <48106812+Y16M035@users.noreply.github.com>
2019-03-01 19:32:56 +0100

    clase bloque
    constructor y getters de cada clase hechos

--- trazabilidad/model/Bloque.java
+++ trazabilidad/model/Bloque.java
@@ -10,10 +10,34 @@ public class Bloque implements Serializable{
     //Constructor por defecto. Asignar tal cual
     //TODO anton
     public Bloque(String hashPrevio, int tipoBloque, int numBloque, int codLote, DatosContainer datos){
-        
+        this.hashPrevio = hashPrevio;
+        this.tipoBloque = tipoBloque;
+        this.numBloque = numBloque;
+        this.codLote = codLote;
+        this.datos = datos;
     }
-
+    
     //Getters para todos los campos
     //TODO anton
+    
+    public  String getHashPrevio(){
+        return this.hashPrevio;
+    }
+    
+    public  int getTipoBloque(){
+        return this.tipoBloque;
+    }
+    
+    public  int getNumBloque(){
+        return this.numBloque;
+    }
+    
+    public  int getCodLote(){
+        return this.codLote;
+    }
+    
+    public  DatosContainer getDatos(){
+        return this.datos;
+    }
 
 }
