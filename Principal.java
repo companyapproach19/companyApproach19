e06fee421e82e0e96b0d47dab63ba27b80c4b828
piuland <piuland@users.noreply.github.com>
2019-03-05 17:48:23 +0100

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -4,7 +4,7 @@ public class Principal {
 	
 	private int contadorLote; //se debe actualizar para crear cada lote
 	
-	public Lote generarPedido(String tipo){
+	/*public Lote generarPedido(String tipo){
 		if (tipo.equals("Stout")) {
 			Lote lote = new Lote(contadorLote);
 			contadorLote++;
@@ -15,7 +15,7 @@ public class Principal {
 			return lote;
 		} else return null;
 		
-	}
+	}*/
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
