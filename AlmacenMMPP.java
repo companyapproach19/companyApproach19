cbda44322083ef34e94889be4c968634f4b3a6cf
piuland <piuland@users.noreply.github.com>
2019-03-05 15:56:13 +0100

    Esqueleto clase almacén

---
+++ AlmacenMMPP.java
@@ -0,0 +1,21 @@
+public class AlmacenMMPP {
+	
+	private double maltaPilsner;
+	private double maltaCaramelo;
+	private double maltaBasePalida;
+	private double maltaMunich;
+	private double maltaNegra;
+	private double maltaCristal;
+	private double maltaChocolate;
+	private double cebadaTostada;
+	private double lupuloPerle;
+	private double lupuloTettnanger;
+	private double lupuloCentennial;
+	private double levaduraAle;
+	private double levaduraLager;
+	
+	public AlmacenMMPP() {
+		
+	}
+
+}
