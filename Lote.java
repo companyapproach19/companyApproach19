1c8b84c108fcb548caed51af8c9d65fc093e95b2
piuland <piuland@users.noreply.github.com>
2019-03-05 17:42:28 +0100

    Clase Principal muy básica y clase Lote, que no importará hasta que tengamos que conectar con el equipo del blockchain

---
+++ Lote.java
@@ -0,0 +1,12 @@
+//package iSoftware;
+
+public class Lote {
+	private int code;
+	private int pedidos[];
+			
+	public Lote(int code) {
+		this.code=code;
+		
+	}
+
+}
---
+++ Principal.java
@@ -0,0 +1,29 @@
+//package iSoftware;
+import java.util.Scanner;
+public class Principal {
+	
+	private int contadorLote; //se debe actualizar para crear cada lote
+	
+	public Lote generarPedido(String tipo){
+		if (tipo.equals("Stout")) {
+			Lote lote = new Lote(contadorLote);
+			contadorLote++;
+			return lote;
+		} else if (tipo.equals("Pilsner")) {
+			Lote lote = new Lote(contadorLote);
+			contadorLote++;
+			return lote;
+		} else return null;
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sca = new Scanner(System.in);
+		System.out.println("Inserte el pedido sobre el que desea consultar: ");
+		String lote = sca.nextLine();
+		int id = Integer.parseInt(lote);
+		
+	}
+
+}
