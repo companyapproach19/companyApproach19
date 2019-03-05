bb14dd50427c431d7dcbf49e83d5e6be46b491e7
LucasBotella <48091728+LucasBotella@users.noreply.github.com>
2019-03-05 20:52:49 +0100

    Add files via upload
    Métodos consulta y inserción:
    -bloqueOrden
    -retailer
    -fabrica

---
+++ Fabrica.java
@@ -0,0 +1,46 @@
+package company;
+
+public class Fabrica {
+	private String CIF;
+	private String direccion;
+	private String idAlmacenMaterias;
+	private String idAlmacenLotes;
+	
+	public Fabrica(String CIF,String direccion,String idAlmacenMaterias,String idAlmacenLotes) {
+		this.CIF= CIF;
+		this.direccion= direccion;
+		this.idAlmacenMaterias=idAlmacenMaterias;
+		this.idAlmacenLotes=idAlmacenLotes;
+	}
+	
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public String getCIF() {
+		return CIF;
+	}
+	public void setCIF(String cIF) {
+		CIF = cIF;
+	}
+
+	public String getIdAlmacenMaterias() {
+		return idAlmacenMaterias;
+	}
+
+	public void setIdAlmacenMaterias(String idAlmacenMaterias) {
+		this.idAlmacenMaterias = idAlmacenMaterias;
+	}
+
+	public String getIdAlmacenLotes() {
+		return idAlmacenLotes;
+	}
+
+	public void setIdAlmacenLotes(String idAlmacenLotes) {
+		this.idAlmacenLotes = idAlmacenLotes;
+	}
+	
+
+}
