e559eeb85affdcb5e426ee4c48513f29a54cd207
MrCabss69 <48258972+MrCabss69@users.noreply.github.com>
2019-03-06 13:43:56 +0100

    Add files via upload

--- Lote.java
+++ Lote.java
@@ -1,12 +1,33 @@
-//package iSoftware;
+package iSoftware;
 
 public class Lote {
-	private int code;
-	private int pedidos[];
-			
-	public Lote(int code) {
-		this.code=code;
-		
+	public static int code;
+	public static String tipo;
+	
+
+	public Lote(Stout name) {
+		Lote.code=name.getId();
+		Lote.tipo="Stout";
+	}
+	
+	public Lote (Pilsner name) {
+		Lote.code=name.getId();
+		Lote.tipo="Pilsner";
 	}
 
+	public static int getCode() {
+		return code;
+	}
+
+	public static void setCode(int code) {
+		Lote.code = code;
+	}
+
+	public static String getTipo() {
+		return tipo;
+	}
+
+	public static void setTipo(String tipo) {
+		Lote.tipo = tipo;
+	}
 }
