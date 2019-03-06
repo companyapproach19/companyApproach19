a1c088aa5e80d5375c08bdb9add21ce059e120d0
Teresa_GCG <t.gomez-carpinterog@alumnos.upm.es>
2019-03-06 13:00:02 +0100

    Actualicado comentarios

--- OrdenTrazabilidad.java
+++ OrdenTrazabilidad.java
@@ -7,6 +7,12 @@ public class OrdenTrazabilidad implements Serializable{
 	 * Esta es la clase que hay que pasar 
 	 * al grupo de trazabilidad
 	 * 
+	 * 
+	 * Estado del proceso
+	 * Emisor
+	 * Destino
+	 * Mensaje
+	 * Firma?
 	 * */
 }
 
