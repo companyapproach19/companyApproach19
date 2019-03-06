bd80c17b41edad3c9347a7547871329181e45660
Teresa_GCG <t.gomez-carpinterog@alumnos.upm.es>
2019-03-06 13:36:51 +0100

    Actualizada con constructor

--- OrdenTrazabilidad.java
+++ OrdenTrazabilidad.java
@@ -14,5 +14,37 @@ public class OrdenTrazabilidad implements Serializable{
 	 * Mensaje
 	 * Firma?
 	 * */
+	
+	//Es necesario un identificador por cada pedido
+	private String id;
+	//No esta seguro que sea necesario el mensaje
+	private String mensaje;
+	//Actor que realiza el pedido
+	private Actor emisor;
+	//Actor que recibe la peticion
+	private Actor receptor;
+	//Estado del pedido
+	private EstadoProceso estado;
+	
+	public void OrdenTrazabilidad(String identificador,String mensaje, Actor emisor, Actor receptor) {
+		this.id = identificador;
+		this.mensaje=mensaje;
+		this.emisor=emisor;
+		this.receptor=receptor;
+		this.estado=null;
+	}
+	
+	public void actualizarEstado(EstadoProceso nuevo) {
+		this.estado=nuevo;
+	}
+	
+	
+	public enum Actor{
+	    PRODUCTOR, COOPERATIVA, FABRICA, RETAILER, TIENDA, TRANSPORTISTA
+	}
+	
+	public enum EstadoProceso{
+		EN_PROCESO, LISTO_PARA_ENTREGAR, EN_PROCESO_DE_ENTREGA, ENTREGADO
+	}
 }
 
