e9fc14c99a0f9c59d8c62700aa1090210cbfb961
Teresa_GCG <t.gomez-carpinterog@alumnos.upm.es>
2019-03-06 14:05:26 +0100

    no message

--- JSONParser.java
+++
@@ -1,7 +0,0 @@
-public class JSONParser {
-
-    /* Esta clase se encarga de pasar de objetos en Java
-     * a un archivo JSON, para la interoperabilidad entre
-     * el backend y la interfaz (en Javascript) */
-
-}
