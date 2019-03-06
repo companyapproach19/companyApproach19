810e0129397519314bd52cd298bd7d17c8008ba8
TeresaGCG <48101212+TeresaGCG@users.noreply.github.com>
2019-03-06 12:38:19 +0100

    Add files via upload

---
+++ OrdenTrazabilidad.java
@@ -0,0 +1,12 @@
+import java.io.Serializable;
+
+public class OrdenTrazabilidad implements Serializable{
+
+	
+	/*
+	 * Esta es la clase que hay que pasar 
+	 * al grupo de trazabilidad
+	 * 
+	 * */
+}
+
