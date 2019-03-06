49ddc6e260b2026b74f5f347b4df3a864a00863f
David Munuera Mazarro <david.mmazarro@alumnos.upm.es>
2019-03-06 12:32:54 +0100

    Creada la clase JSONParser

---
+++ JSONParser.java
@@ -0,0 +1,7 @@
+public class JSONParser {
+
+    /* Esta clase se encarga de pasar de objetos en Java
+     * a un archivo JSON, para la interoperabilidad entre
+     * el backend y la interfaz (en Javascript) */
+
+}
