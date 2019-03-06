ce2f7f326129f2333dfc7b2bbcc28dbb1e1fac76
veronicaarroyo <48092851+veronicaarroyo@users.noreply.github.com>
2019-03-06 12:56:41 +0100

    Add files via upload
    He corregido las clases de las cervezas (Stout y Pilsner) . Las he añadido los getters y setters.

--- Pilsner.java
+++ Pilsner.java
@@ -1,13 +1,13 @@
-package iSoftware;
+package ISO;
 public class Pilsner {
-	private double maltaPilsner;
-	private double maltaCaramelo;
-	private double lupuloPerle;
-	private double lupuloTettnanger;
+	public static double maltaPilsner;
+	public static double maltaCaramelo;
+	public static double lupuloPerle;
+	public static double lupuloTettnanger;
 	//Las levaduras no tienen cantidad, es un error???????????????????????????
-	private boolean levaduraLager;
-	private double agua;
-	private int id;
+	public static boolean levaduraLager;
+	public static double agua;
+	public static int id;
 	
 	public Pilsner() {
 	id=AlmacenMMPP.getId();
@@ -22,6 +22,62 @@ public class Pilsner {
 	lupuloTettnanger = 40;
 	AlmacenMMPP.setLupuloTettnanger(AlmacenMMPP.getLupuloTettnanger()-40);
 	//Falta la levadura
+	}
+
+	public static double getMaltaPilsner() {
+		return maltaPilsner;
+	}
+
+	public static void setMaltaPilsner(double maltaPilsner) {
+		Pilsner.maltaPilsner = maltaPilsner;
+	}
+
+	public static double getMaltaCaramelo() {
+		return maltaCaramelo;
+	}
+
+	public static void setMaltaCaramelo(double maltaCaramelo) {
+		Pilsner.maltaCaramelo = maltaCaramelo;
+	}
+
+	public static double getLupuloPerle() {
+		return lupuloPerle;
+	}
+
+	public static void setLupuloPerle(double lupuloPerle) {
+		Pilsner.lupuloPerle = lupuloPerle;
+	}
+
+	public static double getLupuloTettnanger() {
+		return lupuloTettnanger;
+	}
+
+	public static void setLupuloTettnanger(double lupuloTettnanger) {
+		Pilsner.lupuloTettnanger = lupuloTettnanger;
+	}
+
+	public static boolean isLevaduraLager() {
+		return levaduraLager;
+	}
+
+	public static void setLevaduraLager(boolean levaduraLager) {
+		Pilsner.levaduraLager = levaduraLager;
+	}
+
+	public static double getAgua() {
+		return agua;
+	}
+
+	public static void setAgua(double agua) {
+		Pilsner.agua = agua;
+	}
+
+	public static int getId() {
+		return id;
+	}
+
+	public static void setId(int id) {
+		Pilsner.id = id;
 	}	
 		
 }
--- Stout.java
+++ Stout.java
@@ -1,23 +1,22 @@
-package iSoftware;
+package ISO;
 
 public class Stout {
-	private double maltaCaramelo;
-	private double maltaBasePalida;
-	private double maltaMunich;
-	private double maltaNegra;
-	private double maltaCrystal;
-	private double agua;
-	private double maltaChocolate;
-	private double cebadaTostada;
-	private double lupuloCentennial;
+	public static double maltaCaramelo;
+	public static double maltaBasePalida;
+	public static double maltaMunich;
+	public static double maltaNegra;
+	public static double maltaCrystal;
+	public static double agua;
+	public static double maltaChocolate;
+	public static double cebadaTostada;
+	public static double lupuloCentennial;
 	// Las levaduras no tienen cantidad, es un error???????????????????????????
-	private boolean levaduraAle;
-	private int id;
+	public static boolean levaduraAle;
+	public static int id;
 
 	public Stout() {
-		id = Principal.getid();
-		id++;
-		Principal.setid(id);
+		id = AlmacenMMPP.getId();
+		AlmacenMMPP.setId(AlmacenMMPP.getId() + 1);
 		agua = 24;
 		maltaBasePalida = 5500;
 		AlmacenMMPP.setMaltaBasePalida(AlmacenMMPP.getMaltaBasePalida() - 5500);
@@ -36,6 +35,94 @@ public class Stout {
 		lupuloCentennial = 70;
 		AlmacenMMPP.setLupuloCentennial(AlmacenMMPP.getLupuloCentennial() - 70);
 		// Falta la levadura
+	}
+
+	public static double getMaltaCaramelo() {
+		return maltaCaramelo;
+	}
+
+	public static void setMaltaCaramelo(double maltaCaramelo) {
+		maltaCaramelo = maltaCaramelo;
+	}
+
+	public static double getMaltaBasePalida() {
+		return maltaBasePalida;
+	}
+
+	public static void setMaltaBasePalida(double maltaBasePalida) {
+		maltaBasePalida = maltaBasePalida;
+	}
+
+	public static double getMaltaMunich() {
+		return maltaMunich;
+	}
+
+	public static void setMaltaMunich(double maltaMunich) {
+		maltaMunich = maltaMunich;
+	}
+
+	public static double getMaltaNegra() {
+		return maltaNegra;
+	}
+
+	public static void setMaltaNegra(double maltaNegra) {
+		maltaNegra = maltaNegra;
+	}
+
+	public static double getMaltaCrystal() {
+		return maltaCrystal;
+	}
+
+	public static void setMaltaCrystal(double maltaCrystal) {
+		maltaCrystal = maltaCrystal;
+	}
+
+	public static double getAgua() {
+		return agua;
+	}
+
+	public static void setAgua(double agua) {
+		agua = agua;
+	}
+
+	public static double getMaltaChocolate() {
+		return maltaChocolate;
+	}
+
+	public static void setMaltaChocolate(double maltaChocolate) {
+		maltaChocolate = maltaChocolate;
+	}
+
+	public static double getCebadaTostada() {
+		return cebadaTostada;
+	}
+
+	public static void setCebadaTostada(double cebadaTostada) {
+		cebadaTostada = cebadaTostada;
+	}
+
+	public static double getLupuloCentennial() {
+		return lupuloCentennial;
+	}
+
+	public static void setLupuloCentennial(double lupuloCentennial) {
+		lupuloCentennial = lupuloCentennial;
+	}
+
+	public static boolean isLevaduraAle() {
+		return levaduraAle;
+	}
+
+	public static void setLevaduraAle(boolean levaduraAle) {
+		levaduraAle = levaduraAle;
+	}
+
+	public static int getId() {
+		return id;
+	}
+
+	public static void setId(int id) {
+		id = id;
 	}
 
 }
