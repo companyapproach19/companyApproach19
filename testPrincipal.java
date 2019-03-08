d55dc629f4296740f910e8ed0926922065a7be83
nataliagila <48092814+nataliagila@users.noreply.github.com>
2019-03-08 13:44:56 +0100

    Add files via upload
    subo una actualización de los tests, no entiendo los errores pero el modelo de los tests es el correcto

--- testPrincipal.java
+++ testPrincipal.java
@@ -9,18 +9,32 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import iSoftware.Principal;
+import iSoftware.Lote;
 
 public class testPrincipal {
 	@Test
 	void test1() {
-		int result = Principal.moler();
+		Lote lote= new Lote(new Pilsner());
+		int result = Principal.moler(lote);
 		assertEquals(result, 0);
 	}
 	@Test
 	void test2() {
-		int result = Principal.cocinar();
+		int result = Principal.cocinar(new Lote(new Pilsner()));
 		assertEquals(result, 0);
 	}
+	@Test
+	void test3() {
+		Lote lote = new Lote(new Stout());
+		int result = Principal.cocinar(lote);
+		assertEquals(result, 0);
+	}
+	void test4() {
+		Lote lote= new Lote(new Stout());
+		int result = Principal.moler(lote);
+		assertEquals(result, 0);
+	}
+	
 	
 	
 }
