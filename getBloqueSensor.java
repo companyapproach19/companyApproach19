c774275b91fcc07e2a0f4e6bbddcd5f4786fbfc4
Rickkk2 <48155271+Rickkk2@users.noreply.github.com>
2019-03-08 10:01:31 +0100

    Update getBloqueSensor.java

--- getBloqueSensor.java
+++ getBloqueSensor.java
@@ -5,7 +5,7 @@
      */
     private Bloque getBloqueSensor (int tipoBloque, int numBloque) throws SQLException, ClassNotFoundException{
     	conectar();
-    	String query = "SELECT * FROM company.bloqueSensor WHERE tipoBloque = " + tipoBloque + " AND numBloque = " + numBloque + ";");
+    	String query = "SELECT * FROM company.bloqueSensor WHERE tipoBloque = " + tipoBloque + " AND numBloque = " + numBloque + ";";
     	PreparedStatement pst = conn.prepareStatement(query);
     	ResultSet rs = pst.executeQuery(query);
     	Bloque buscado = new Bloque(rs.getString(1), rs.getInt(2), rs.getInt(3), rs.getInt(4), rs.getObject(5));
