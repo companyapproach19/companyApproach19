5f7a4941703a130a89359004715c7ece5920583f
piuland <piuland@users.noreply.github.com>
2019-03-08 13:29:29 +0100

    Add files via upload

--- Lote.java
+++ Lote.java
@@ -1,18 +1,31 @@
 package iSoftware;
 
+import java.util.*;
+
 public class Lote {
 	public static int code;
 	public static String tipo;
-	
+	public static LinkedList<String> pedidos;
+	private static Date fecha_inicio;
 
 	public Lote(Stout name) {
 		Lote.code=name.getId();
 		Lote.tipo="Stout";
+		pedidos = new LinkedList<String>();
 	}
 	
 	public Lote (Pilsner name) {
 		Lote.code=name.getId();
 		Lote.tipo="Pilsner";
+		pedidos = new LinkedList<String>();
+	}
+
+	public static LinkedList<String> getPedidos() {
+		return pedidos;
+	}
+
+	public static void setPedidos(LinkedList<String> pedidos) {
+		Lote.pedidos = pedidos;
 	}
 
 	public static int getCode() {
@@ -29,5 +42,13 @@ public class Lote {
 
 	public static void setTipo(String tipo) {
 		Lote.tipo = tipo;
+	}
+
+	public static Date getFecha_inicio() {
+		return fecha_inicio;
+	}
+
+	public static void setFecha_inicio(Date fecha_inicio) {
+		Lote.fecha_inicio = fecha_inicio;
 	}
 }
