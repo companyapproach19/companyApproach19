d81eeb791b3cf8d2e152c8c4ed7f90029e5ac677
Rickkk2 <48155271+Rickkk2@users.noreply.github.com>
2019-03-08 10:05:02 +0100

    Create getLote.java

---
+++ getLote.java
@@ -0,0 +1,11 @@
+    private Lote getLote (int code) {
+    	conectar();
+    	String query = "SELECT * FROM company.lote WHERE code  = " + code + ";";
+    	PreparedStatement pst = conn.prepareStatement(query);
+    	ResultSet rs = pst.executeQuery(query);
+    	Lote buscado = new Lote(rs.getInt(1), rs.getString(2));
+    	pst.close();
+		rs.close();
+    	conn.close();
+    	return buscado;
+    }
