5d79c551298249fec6bc6800648992cd6ad61262
piuland <piuland@users.noreply.github.com>
2019-03-08 12:20:22 +0100

    Add files via upload

--- Principal.java
+++ Principal.java
@@ -1,4 +1,4 @@
-package ISO;
+package iSoftware;
 
 import java.util.Scanner;
 
@@ -7,25 +7,25 @@ public class Principal {
 	@SuppressWarnings("deprecation")
 	private static void moler() throws InterruptedException {
 		java.util.Date fechaActual = new java.util.Date();
-		System.out.println("Día: " + fechaActual.getDate());
+		System.out.println("Dï¿½a: " + fechaActual.getDate());
 		System.out.println("Moliendo...");
 		Thread.sleep(3000);
 		fechaActual.setDate(fechaActual.getDate() + 1);
-		System.out.println("Proceso de molienda finalizado. Día: " + fechaActual.getDate());
+		System.out.println("Proceso de molienda finalizado. Dï¿½a: " + fechaActual.getDate());
 	}
 	
 	@SuppressWarnings("deprecation")
 	private static void cocinar() throws InterruptedException {
 		java.util.Date fechaActual = new java.util.Date();
-		System.out.println("Día: "+fechaActual.getDate());
+		System.out.println("Dï¿½a: "+fechaActual.getDate());
 		System.out.println("Moliendo...");
 		Thread.sleep(3000);
 		fechaActual.setDate(fechaActual.getDate()+1); 
-		System.out.println("Proceso de molienda finalizado. Día: "+fechaActual.getDate());
+		System.out.println("Proceso de molienda finalizado. Dï¿½a: "+fechaActual.getDate());
 	}
 
 	public static void main(String[] args) throws InterruptedException {
-		System.out.println("¿Desea generar un lote? (s/n)");
+		System.out.println("ï¿½Desea generar un lote? (s/n)");
 		Scanner sc = new Scanner(System.in);
 		String answ = sc.nextLine();
 		switch(answ.toLowerCase()) {
@@ -34,21 +34,27 @@ public class Principal {
 				System.err.println("Alerta: El almacen de lotes esta completo. Recuerde que no podra almacenar el lote que va a producir si no vacia el almacen");
 			}
 			else {
-			System.out.println("Comienza la fase de molienda");
+			System.out.println("Comienza la fase de molienda.");
 			moler();
-			System.out.println("Comienza la fase de cocinado");
+			System.out.println("Comienza la fase de cocinado.");
 			cocinar();
+			/*System.out.println("Comienza la fase de fermentaciÃ³n.");
+			fermentar();
+			System.out.println("Comienza la segunda fase de fermentaciÃ³n.");
+			fermentar();
+			System.out.println("Comienza la fase de embotellado.");
+			embotellar();
+			*/
 			}
 			break;
 		case "n":
 			Scanner sca = new Scanner(System.in);
-			System.out.println("Inserte el pedido sobre el que desea consultar: ");
+			System.out.println("Inserte el lote sobre el que desea consultar: ");
 			String lote = sca.nextLine();
 			int id = Integer.parseInt(lote);
+			if (id > AlmacenLotes.id) System.err.println("Lote introducido incorrecto.");
 			break;
 		}
-			
-
 
 	}
 
