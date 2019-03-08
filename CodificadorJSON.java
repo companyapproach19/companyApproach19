58ffbdd0ab4b54d865eca9bf430b61c13af38197
David Munuera Mazarro <david.mmazarro@alumnos.upm.es>
2019-03-08 20:56:58 +0100

    Hecho el codificador que crea JSON a partir de las peticiones

--- CodificadorJSON.java
+++ CodificadorJSON.java
@@ -1,5 +1,6 @@
-// import com.google.gson.*
-
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.io.IOException;
 
 public class CodificadorJSON {
 
@@ -7,6 +8,10 @@ public class CodificadorJSON {
      * a un archivo JSON, para la interoperabilidad entre
      * el backend y la interfaz (en Javascript) */
 
-
+    public static String crearJSON(OrdenTrazabilidad pedido) {
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.setPrettyPrinting().create();
+        return gson.toJson(pedido);
+    }
 
 }
