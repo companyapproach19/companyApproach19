8c4d9c47b2d274b1e352803bcca5f9975d46620f
Teresa_GCG <t.gomez-carpinterog@alumnos.upm.es>
2019-03-08 17:52:10 +0100

    Eliminacion de la clase Persona y creacion de la clase para descodificar los json En comentarios de la nueva clase pone una idea de lo que necesitan las otras clases

---
+++ DescodificadorJson.java
@@ -0,0 +1,33 @@
+
+public class DescodificadorJson {
+	
+	public DescodificadorJson() {
+		
+	}
+
+	
+	/*La clase OrdenTrazabilidad necesita de esta clase:
+	 * 
+	 * Toda la informacion inicial del pedido :)
+	 * Modificaciones del estado del proceso
+	 * Fecha de entrega (cuando el transportista firme la entrega)
+	 * 
+	 */
+	
+	/*Las clases individuales de los actores necesitaran de esta clase:
+	 * 
+	 * Agricultor:
+	 * Boolean de si se ha aceptado el pedido o no
+	 * Boolean de si le da al boton de: listo
+	 * ...
+	 * 
+	 * Resto de actores:
+	 * Boolean de si le da al boton de: listo para entregar
+	 * Los actores que puedan hacer peticiones necesitaran saber el mensaje
+	 * ...
+	 * 
+	 */
+	
+	
+	
+}
