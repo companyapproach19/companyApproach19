b8d48f2a67204267e78ffccf1e170951cb49ebc3
Maria-Larrios-Cuenca <48140561+Maria-Larrios-Cuenca@users.noreply.github.com>
2019-03-10 16:24:57 +0100

    Update getTransportista.java

--- getTransportista.java
+++ getTransportista.java
@@ -3,8 +3,8 @@
       *@param CIF del transportisa
       *@return transportista
       */
-    private Transportista getTransportista (String CIF) throws SQLException{
-    	String query = "SELECT * FROM company.transportista WHERE transportista.CIF = " + CIF + ";"; 
+    private Transportista getTransportista (String transportista_cif) throws SQLException{
+    	String query = "SELECT * FROM company.transportista WHERE transportista.transportista_cif = " + transportista_cif + ";"; 
     	PreparedStatement pst = conn.prepareStatement(query);
     	ResultSet rs = pst.executeQuery(query);
     	Transportista buscado = new Transportista(rs.getString(1), rs.getString(2));
