4bfeabff15364df58ca7ae41b351a76a387f3220
felipeVazq <48183031+felipeVazq@users.noreply.github.com>
2019-03-10 16:37:14 +0100

    Add files via upload

--- company.java
+++ company.java
@@ -1,3 +1,5 @@
+package beerCompany;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -11,7 +13,6 @@ public class company {
 /*    private String dbUrl = "jdbc:postgresql://ec2-54-197-232-203.compute-1.amazonaws.com:5432/da8thb0c81jj6n?user=voamftsogizhrl&password=b92c40a06c23bf20ef80f4270ebf62bd464e9432d65e38458e047b7597bd5446&sslmode=require";
     private static String dbUrl= System.getenv("dburl");
     private static Connection connection= null;
-
     private static void conectar(){
         try {
             connection= DriverManager.getConnection(dbUrl);
@@ -34,14 +35,14 @@ public class company {
     private  void exit() throws Exception {
         //cierre de la conexion
         if (conn!=null) this.conn.close();
-        System.out.println("Saliendo.. Â¡hasta otra!");
+        System.out.println("Saliendo.. ¡hasta otra!");
         System.exit(0);
     }
 
     private static void crearBD()   throws Exception {
         PreparedStatement pst = conn.prepareStatement("CREATE SCHEMA `company`;");
         pst.executeUpdate();
-        System.out.println("Â¡Base de datos Creada!");
+        System.out.println("¡Base de datos Creada!");
 
         //creacion de las tablas
         PreparedStatement pst1 = conn.prepareStatement(
@@ -128,6 +129,57 @@ public class company {
                         ") ENGINE=InnoDB;"
         );
         pst5.executeUpdate();
+        
+        PreparedStatement pst6 = conn.prepareStatement(
+                "CREATE TABLE `company`.`bloque` (" +
+                        "`hashPrevio` VARCHAR(250) NOT NULL ," +
+                        "`tipoBloque` INT NOT NULL, " +
+                        "`numBloque` INT NOT NULL ," +
+                        "`codLote` INT NOT NULL, " +
+                        "`datos` BLOB NOT NULL, " +
+                        "PRIMARY KEY (`hashPrevio`)) ENGINE=InnoDB;"
+        );
+        pst6.executeUpdate();
+        
+        PreparedStatement pst7 = conn.prepareStatement(
+                "CREATE TABLE `company`.`agricultor` (" +
+                        "`cif` VARCHAR(45) NOT NULL ," +
+                        "`nombre` VARCHAR(45) NOT NULL, " +
+                        "`direccion` VARCHAR(250) NOT NULL ," +
+                        "`cifCooperativa` INT NOT NULL, " +
+                        "PRIMARY KEY (`cif`)) ENGINE=InnoDB;"
+        );
+        pst7.executeUpdate();
+        
+        PreparedStatement pst8 = conn.prepareStatement(
+                "CREATE TABLE `company`.`cooperativa` (" +
+                		"`cif` VARCHAR(45) NOT NULL ," +
+                        "`nombre` VARCHAR(45) NOT NULL, " +
+                        "`direccion` VARCHAR(250) NOT NULL ," +
+                        "PRIMARY KEY (`cif`)) ENGINE=InnoDB;"
+        );
+        pst8.executeUpdate();
+        
+        PreparedStatement pst9 = conn.prepareStatement(
+                "CREATE TABLE `company`.`retailer` (" +
+                        "`cif` VARCHAR(45) NOT NULL ," +
+                        "`nombre` VARCHAR(45) NOT NULL, " +
+                        "`direccion` VARCHAR(250) NOT NULL ," +
+                        "PRIMARY KEY (`cif`)) ENGINE=InnoDB;"
+        );
+        pst9.executeUpdate();
+        
+        PreparedStatement pst10 = conn.prepareStatement(
+                "CREATE TABLE `company`.`actor` (" +
+                        "`nombreUsuario` VARCHAR(45) NOT NULL ," +
+                        "`passwdPlana` VARCHAR(45) NOT NULL, " +
+                        "`passwdSalt` VARCHAR(45) NOT NULL ," +
+                        "`email` VARCHAR(250) NOT NULL, " +
+                        "`usuarioPrevio` BLOB NOT NULL, " +
+                        "`tipoActor` INT NOT NULL, " +
+                        "PRIMARY KEY (`nombreUsuario`)) ENGINE=InnoDB;"
+        );
+        pst10.executeUpdate();
     }
 
         public  void main (String[]args){
