0540f273ca05407904d23e28017dccee0fcd77a5
LucasBotella <48091728+LucasBotella@users.noreply.github.com>
2019-03-10 20:51:55 +0100

    Update company.java

--- equipo5/dao/company.java
+++ equipo5/dao/company.java
@@ -46,84 +46,84 @@ public class company {
 
         //creacion de las tablas
         PreparedStatement pst1 = conn.prepareStatement(
-                "CREATE TABLE `company`.`transportista` (" +
-                        "`transportista_cif` VARCHAR(45) NOT NULL ," +
-                        "`empresa_transporte` VARCHAR(45) NOT NULL, " +
-                        "PRIMARY KEY (`transportista_cif`)) ENGINE=InnoDB;"
+                "CREATE TABLE 'company'.'transportista' (" +
+                        "'transportista_cif' VARCHAR(45) NOT NULL ," +
+                        "'empresa_transporte' VARCHAR(45) NOT NULL, " +
+                        "PRIMARY KEY ('transportista_cif')) ENGINE=InnoDB;"
         );
         pst1.executeUpdate();
 
         PreparedStatement pst2 = conn.prepareStatement(
-                "CREATE TABLE `company`.`almacenMateriasPrimas` (" +
-                        "`idAlmacenMateriasPrimas` INT NOT NULL, " +
-                        "`capacidad` DOUBLE NULL, " +
-                        "`maltaPilsner` DOUBLE NULL, " +
-                        "`maltaCaramelo` DOUBLE NULL, " +
-                        "`maltaBasePalida` DOUBLE NULL, " +
-                        "`maltaMunich` DOUBLE NULL, " +
-                        "`maltaNegra` DOUBLE NULL, " +
-                        "`maltaCrystal` DOUBLE NULL, " +
-                        "`maltaChocolate` DOUBLE NULL, " +
-                        "`cebadaTostada` DOUBLE NULL, " +
-                        "`lupuloPerle` DOUBLE NULL, " +
-                        "`lupuloTettnanger` DOUBLE NULL, " +
-                        "`lupuloCentennial` DOUBLE NULL, " +
-                        "`levaduraAle` DOUBLE NULL, " +
-                        "`levaduraLager` DOUBLE NULL, " +
-                        "`maxCapacidad` DOUBLE NULL, " +
-                        "`maxMaltaPilsner` DOUBLE NULL, " +
-                        "`maxMaltaCaramelo` DOUBLE NULL, " +
-                        "`maxMaltaBasePalida` DOUBLE NULL, " +
-                        "`maxMaltaMunich` DOUBLE NULL, " +
-                        "`maxMaltaNegra` DOUBLE NULL, " +
-                        "`maxMaltaCrystal` DOUBLE NULL, " +
-                        "`maxMaltaChocolate` DOUBLE NULL, " +
-                        "`maxCebadaTostada` DOUBLE NULL, " +
-                        "`maxLupuloPerle` DOUBLE NULL, " +
-                        "`maxLupuloTettnanger` DOUBLE NULL, " +
-                        "`maxLupuloCentennial` DOUBLE NULL, " +
-                        "`maxLevaduraAle` DOUBLE NULL, " +
-                        "`maxLevaduraLager` DOUBLE NULL, " +
-                        "PRIMARY KEY (`idalmacenMateriasPrimas`));"
+                "CREATE TABLE 'company'.'almacenMateriasPrimas' (" +
+                        "'idAlmacenMateriasPrimas' INT NOT NULL, " +
+                        "'capacidad' DOUBLE NULL, " +
+                        "'maltaPilsner' DOUBLE NULL, " +
+                        "'maltaCaramelo' DOUBLE NULL, " +
+                        "'maltaBasePalida' DOUBLE NULL, " +
+                        "'maltaMunich' DOUBLE NULL, " +
+                        "'maltaNegra' DOUBLE NULL, " +
+                        "'maltaCrystal' DOUBLE NULL, " +
+                        "'maltaChocolate' DOUBLE NULL, " +
+                        "'cebadaTostada' DOUBLE NULL, " +
+                        "'lupuloPerle' DOUBLE NULL, " +
+                        "'lupuloTettnanger' DOUBLE NULL, " +
+                        "'lupuloCentennial' DOUBLE NULL, " +
+                        "'levaduraAle' DOUBLE NULL, " +
+                        "'levaduraLager' DOUBLE NULL, " +
+                        "'maxCapacidad' DOUBLE NULL, " +
+                        "'maxMaltaPilsner' DOUBLE NULL, " +
+                        "'maxMaltaCaramelo' DOUBLE NULL, " +
+                        "'maxMaltaBasePalida' DOUBLE NULL, " +
+                        "'maxMaltaMunich' DOUBLE NULL, " +
+                        "'maxMaltaNegra' DOUBLE NULL, " +
+                        "'maxMaltaCrystal' DOUBLE NULL, " +
+                        "'maxMaltaChocolate' DOUBLE NULL, " +
+                        "'maxCebadaTostada' DOUBLE NULL, " +
+                        "'maxLupuloPerle' DOUBLE NULL, " +
+                        "'maxLupuloTettnanger' DOUBLE NULL, " +
+                        "'maxLupuloCentennial' DOUBLE NULL, " +
+                        "'maxLevaduraAle' DOUBLE NULL, " +
+                        "'maxLevaduraLager' DOUBLE NULL, " +
+                        "PRIMARY KEY ('idalmacenMateriasPrimas'));"
         );
         pst2.executeUpdate();
 
         PreparedStatement pst3 = conn.prepareStatement(
-                "CREATE TABLE `company`.`lote` (" +
-                        "`idLote` INT NOT NULL ," +
-                        "`tipo` VARCHAR(45) NOT NULL, " +
-                        "`pedido` VARCHAR(45) NOT NULL ," +
-                        "`fecha_inicio` DATETIME NOT NULL ," +
-                        "`fecha_final` DATETIME NOT NULL, " +
-                        "PRIMARY KEY (`idLote`)) ENGINE=InnoDB;"
+                "CREATE TABLE 'company'.'lote' (" +
+                        "'idLote' INT NOT NULL ," +
+                        "'tipo' VARCHAR(45) NOT NULL, " +
+                        "'pedido' VARCHAR(45) NOT NULL ," +
+                        "'fecha_inicio' DATETIME NOT NULL ," +
+                        "'fecha_final' DATETIME NOT NULL, " +
+                        "PRIMARY KEY ('idLote')) ENGINE=InnoDB;"
         );
         pst3.executeUpdate();
 
         PreparedStatement pst4 = conn.prepareStatement(
-                "CREATE TABLE `company`.`almacenLotes` (" +
-                        "`idAlmacenLotes` INT NOT NULL ," +
-                        "`ocupado` INT NOT NULL, " +
-                        "`maxCapacidad` INT NOT NULL ," +
-                        "`lotes` BLOB NOT NULL, " +
-                        "PRIMARY KEY (`idAlmacenLotes`)) ENGINE=InnoDB;"
+                "CREATE TABLE 'company'.'almacenLotes' (" +
+                        "'idAlmacenLotes' INT NOT NULL ," +
+                        "'ocupado' INT NOT NULL, " +
+                        "'maxCapacidad' INT NOT NULL ," +
+                        "'lotes' BLOB NOT NULL, " +
+                        "PRIMARY KEY ('idAlmacenLotes')) ENGINE=InnoDB;"
         );
         pst4.executeUpdate();
 
         PreparedStatement pst5 = conn.prepareStatement(
-                "CREATE TABLE `company`.`fabrica` (" +
-                        "`fabrica_cif` VARCHAR(45) NOT NULL ," +
-                        "`direccion` VARCHAR(250) NOT NULL, " +
-                        "`idAlmacenMaterias` INT NOT NULL, " +
-                        "`idAlmacenLotes` INT NOT NULL, " +
-                        "PRIMARY KEY (`fabrica_cif`), " +
-                        "CONSTRAINT `fk_fabrica_1` " +
-                        "  FOREIGN KEY (`idAlmacenMaterias`) " +
-                        "  REFERENCES `company`.`almacenMateriasPrimas`(`idAlmacenMateriasPrimas`) " +
+                "CREATE TABLE 'company'.'fabrica' (" +
+                        "'fabrica_cif' VARCHAR(45) NOT NULL ," +
+                        "'direccion' VARCHAR(250) NOT NULL, " +
+                        "'idAlmacenMaterias' INT NOT NULL, " +
+                        "'idAlmacenLotes' INT NOT NULL, " +
+                        "PRIMARY KEY ('fabrica_cif'), " +
+                        "CONSTRAINT 'fk_fabrica_1' " +
+                        "  FOREIGN KEY ('idAlmacenMaterias') " +
+                        "  REFERENCES 'company'.'almacenMateriasPrimas'('idAlmacenMateriasPrimas') " +
                         "  ON DELETE NO ACTION " +
                         "  ON UPDATE NO ACTION, " +
-                        "CONSTRAINT `fk_fabrica_2` " +
-                        "  FOREIGN KEY (`idAlmacenLotes`) " +
-                        "  REFERENCES `company`.`almacenLotes`(`idAlmacenLotes`) " +
+                        "CONSTRAINT 'fk_fabrica_2' " +
+                        "  FOREIGN KEY ('idAlmacenLotes') " +
+                        "  REFERENCES 'company'.'almacenLotes'('idAlmacenLotes') " +
                         "  ON DELETE NO ACTION " +
                         "  ON UPDATE NO ACTION " +
                         ") ENGINE=InnoDB;"
