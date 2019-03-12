9d9d3efeb80545b07772ffa96be0edb722438f4b
Marisol Barrientos Moreno <ms28@hotmail.es>
2019-03-12 19:39:14 +0100

    Añadir comentarios

--- equipo8/test/PruebasSensor.java
+++ equipo8/test/PruebasSensor.java
@@ -4,12 +4,15 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
+//Clase donde se simulan posibles llamadas al método "buscar" que se encuentra en la clase"sensorPrueba"
 public class PruebasSensor extends DatosContainer{
 	
+	//Se crea un nuevo sensor
 	private static Sensor sensorPrueba= new Sensor();
 	
 	public static void main (String args[]) throws IOException {
 		
+		// El método "buscar" recibe como parámetros fechas ficticias, que serán buscadas dentro de un log real
 		System.out.println("Prueba 1   Fecha correcta Temperatura:"+sensorPrueba.buscar("2019","3","10","16","15","12"));
 		System.out.println("Prueba 2   Fecha correcta Temperatura:"+sensorPrueba.buscar("2019","3","10","16","15","33"));
 		System.out.println("Prueba 3   Fecha incorrecta Temperatura:"+sensorPrueba.buscar("2019","4","10","16","15","53"));
